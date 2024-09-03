3040b678f3d7ffecb8c09c03e31e6c03b471e233
KOU050223 <kouhei20050223@icloud.com>
2024-09-04 00:30:13 +0900

    同一LANから接続できるように変更

--- apps/app.py
+++ apps/app.py
@@ -22,4 +22,4 @@ def handle_post():
     return jsonify({"message": "データを受け取りました"}), 200
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True,host='0.0.0.0',port=5000)
