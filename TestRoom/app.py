3aecb6fbb97886e53a8d20fb9b7511d98d9f0bed
KOU050223 <kouhei20050223@icloud.com>
2024-10-10 00:05:38 +0900

    wip 保存

--- TestRoom/app.py
+++ TestRoom/app.py
@@ -14,54 +14,108 @@ rooms_actions = {}  # 各ルームのプレイヤーのアクションを管理
 
 # Blueprintの作成
 samplesite = Blueprint(
-    "sampleSite",
+    "app",
     __name__,
     template_folder="templates",
     static_folder="static"
 )
 
 # ロビーのルート
-@samplesite.route('/')
-def lobby():
-    # 満員でない部屋だけを取り出す
-    ava_rooms = {room: users for room, users in rooms.items() if len(users) < 2}
-    return render_template('lobby.html', rooms=ava_rooms)
+# @samplesite.route('/')
+# def lobby():
+#     # 満員でない部屋だけを取り出す
+#     ava_rooms = {room: users for room, users in rooms.items() if len(users) < 2}
+#     return render_template('lobby.html', rooms=ava_rooms)
 
-# ルーム作成のルート
-@samplesite.route('/create_room', methods=['POST'])
-def create_room():
-    room_name = request.form['room_name']
-    if room_name not in rooms:
-        rooms[room_name] = []
-    return redirect(url_for('sampleSite.join_room_view', room_name=room_name))
-
+# # ルーム作成のルート
 # @samplesite.route('/create_room', methods=['POST'])
-
 # def create_room():
-#     room_name = request.json.get('room_name')
+#     room_name = request.form['room_name']
 #     if room_name not in rooms:
 #         rooms[room_name] = []
-#     return jsonify({'status': 'Room created', 'room_name': room_name})
+#     return redirect(url_for('sampleSite.join_room_view', room_name=room_name))
 
-@app.route('/rooms', methods=['GET'])
-def get_rooms():
-    return jsonify(list(rooms.keys()))
+# # @samplesite.route('/create_room', methods=['POST'])
+
+# # def create_room():
+# #     room_name = request.json.get('room_name')
+# #     if room_name not in rooms:
+# #         rooms[room_name] = []
+# #     return jsonify({'status': 'Room created', 'room_name': room_name})
+
+# @app.route('/rooms', methods=['GET'])
+# def get_rooms():
+#     return jsonify(list(rooms.keys()))
+
+# @socketio.on('create_room')
+# def on_create_room(data):
+#     room_name = data['room_name']
+#     if room_name not in rooms:
+#         rooms[room_name] = []
+#         # 新しいルームが作成されたことをすべてのクライアントに通知
+#         emit('room_list_update', {'room_name': room_name}, broadcast=True)
+
+# # ルーム参加のルート
+# @samplesite.route('/room/<room_name>')
+# def join_room_view(room_name):
+#     if room_name in rooms and len(rooms[room_name]) < 2:
+#         return render_template('room.html', room_name=room_name)
+#     else:
+#         return redirect(url_for('sampleSite.lobby'))
+
+# @socketio.on('join')
+# def on_join(data):
+#     room_name = data['room']
+#     username = data['username']
+
+#     if room_name in rooms and len(rooms[room_name]) < 2:
+#         join_room(room_name)
+#         rooms[room_name].append(username)
+#         emit('status', {'msg': f'{username} has joined the room.'}, room=room_name)
+#         emit('room_info', {'users': rooms[room_name]}, room=room_name)
+#     else:
+#         emit('status', {'msg': 'Room is full.'})
+
+# @socketio.on('leave')
+# def on_leave(data):
+#     room_name = data['room']
+#     username = data['username']
+
+#     leave_room(room_name)
+#     rooms[room_name].remove(username)
+#     emit('status', {'msg': f'{username} has left the room.'}, room=room_name)
+
+# -----------------------------------------------------------------
+
+@socketio.on('message')
+def handle_message(data):
+    room_name = data['room']
+    msg = data['msg']
+    username = data['username']
+    emit('message', {'msg': f'{username}:{msg}'})
+
+
+@socketio.on('connect')
+def test_connect():
+    print('クライアントが接続しました')
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('クライアントが切断されました')
+
+
+@app.route('/play', methods=['POST'])  # POSTメソッドを許可
+def handle_post():
+    data = request.json  # 受信したJSONデータを取得
+    print(data)
+    return jsonify({"message": "データを受け取りました"}), 200
 
 @socketio.on('create_room')
 def on_create_room(data):
     room_name = data['room_name']
     if room_name not in rooms:
         rooms[room_name] = []
-        # 新しいルームが作成されたことをすべてのクライアントに通知
         emit('room_list_update', {'room_name': room_name}, broadcast=True)
-
-# ルーム参加のルート
-@samplesite.route('/room/<room_name>')
-def join_room_view(room_name):
-    if room_name in rooms and len(rooms[room_name]) < 2:
-        return render_template('room.html', room_name=room_name)
-    else:
-        return redirect(url_for('sampleSite.lobby'))
 
 @socketio.on('join')
 def on_join(data):
@@ -71,32 +125,19 @@ def on_join(data):
     if room_name in rooms and len(rooms[room_name]) < 2:
         join_room(room_name)
         rooms[room_name].append(username)
-        emit('status', {'msg': f'{username} has joined the room.'}, room=room_name)
-        emit('room_info', {'users': rooms[room_name]}, room=room_name)
+        emit('status', {'msg': f'{username} has joined the room.'})
+        emit('room_info', {'users': rooms[room_name]})
     else:
         emit('status', {'msg': 'Room is full.'})
 
-@socketio.on('leave')
-def on_leave(data):
-    room_name = data['room']
-    username = data['username']
 
-    leave_room(room_name)
-    rooms[room_name].remove(username)
-    emit('status', {'msg': f'{username} has left the room.'}, room=room_name)
 
-@socketio.on('message')
-def handle_message(data):
-    room_name = data['room']
-    msg = data['msg']
-    username = data['username']
-    emit('message', {'msg': f'{username}:{msg}'}, room=room_name)
 
-@app.route('/play', methods=['POST'])  # POSTメソッドを許可
-def handle_post():
-    data = request.json  # 受信したJSONデータを取得
-    print(data)
-    return jsonify({"message": "データを受け取りました"}), 200
+
+
+
+
+
 
 @socketio.on('player_action')
 def handle_player_action(data):
@@ -112,7 +153,7 @@ def handle_player_action(data):
     rooms_actions[room_name][username] = action
     print("データ",room_name,username,rooms_actions[room_name][username])
     # アクション確認メッセージを送信
-    emit('action_confirmation', {'username': username, 'action': action}, room=room_name)
+    emit('action_confirmation', {'username': username, 'action': action})
 
     # 両プレイヤーがアクションを入力しているか確認
     if len(rooms_actions[room_name]) == 2:
